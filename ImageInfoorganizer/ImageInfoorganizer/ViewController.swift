b1b6a9cb4085567a26e8a945169b15dd6b3691a0
Sai Anusha <saianusha021@gmail.com>
2020-02-04 23:02:09 -0500

    Created a new Project in xcode

---
+++ ImageInfoorganizer/ImageInfoorganizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageInfoorganizer
+//
+//  Created by Anusha Meda on 2/4/20.
+//  Copyright © 2020 Anusha Meda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
